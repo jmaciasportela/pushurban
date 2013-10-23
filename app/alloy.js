13811ca259d370e05aa6179529c0639eab7fd505
Jesus Macias <jmacias@full-on-net.com>
2013-10-23 10:31:03 +0200

    Add base project

--- app/alloy.js
+++ app/alloy.js
@@ -9,3 +9,47 @@
 // object. For example:
 //
 // Alloy.Globals.someGlobalFunction = function(){};
+
+
+
+var urbanairport = require('urbanairport');
+ 
+urbanairport.register({
+  debug: true, // Show debug info 
+  // Sets push types
+  sound: true,   // iOS + Android (default)
+  vibrate: true, // Android (default)
+  badge: true,   // iOS (default)
+  alert: true,   // iOS (default) 
+  // Set any property and call any single-property method of the extended module
+  autoBadge: false, 
+  // Enable compatibility mode (see below)
+  compatibility: true, 
+  // On Android these will be automatically set once UA is flying
+  alias: 'John',
+  tags: 'single', // Supports both a single or Array of strings! 
+  callback: function(e) { // The only callback you need  
+    // Registration failed
+    if (e.type === 'error') {
+      alert('Sorry, no push for you: ' + e.error); 
+    // Registration done
+    } else if (e.type === 'success') {
+      alert('Your token is: ' + e.deviceToken); 
+    // Received notification
+    } else if (e.type === 'callback') { 
+      // Properties are normalized for iOS and Android:
+      // e.payload === e.data === e.data.aps
+      // e.message === e.data.alert === e.data.aps.alert
+      alert(e.message);
+    }
+  }
+});
+ 
+// Manually disable/re-enable push
+// urbanairport.enable(); // disable();
+ 
+// Append tags instead of resetting them
+urbanairport.addTags('foo'); // Both single and Array supported
+ 
+// Set any property and call any single-property method of the extended module
+urbanairport.showOnAppClick = true;
